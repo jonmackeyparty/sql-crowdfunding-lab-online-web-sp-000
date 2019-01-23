0b8f1ee5238241ec5b8bd5d16ba23fb8eba93d76
Jon Mackey <jon@jonmackey.party>
2019-01-23 17:15:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,7 +29,7 @@ INSERT INTO users (id, name, age) VALUES
 INSERT INTO users (id, name, age) VALUES
 (15, 'Albus', 113);
 INSERT INTO users (id, name, age) VALUES
-
+(16, 'Squid', 5);
 INSERT INTO users (id, name, age) VALUES
 
 INSERT INTO users (id, name, age) VALUES
