bf6696c7575de7e5831ac31c8d791accdc784f71
Jon Mackey <jon@jonmackey.party>
2019-01-23 18:03:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,8 +27,8 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, amount FROM pledges
 INNER JOIN pledges ON pledges.user_id = users.id
-GROUP BY projects.id
-ORDER BY projects.title;"
+GROUP BY users.name
+ORDER BY amount, users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
