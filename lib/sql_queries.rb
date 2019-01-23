57946552d859b4fe46d2ed4f74b0fe5b89bdda7d
Jon Mackey <jon@jonmackey.party>
2019-01-23 18:46:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,8 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT title, (SUM(pledges.amount) - funding_goal) AS amnt_over FROM projects 
 INNER JOIN pledges ON projects.id = pledges.project_id
 GROUP BY pledges.project_id
-ORDER BY amnt_over, title
-LIMIT 2;"
+ORDER BY amnt_over, title;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
