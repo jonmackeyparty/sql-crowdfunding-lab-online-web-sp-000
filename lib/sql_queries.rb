cbbaed60e8115e9bbed2cf4982958682096c5a46
Jon Mackey <jon@jonmackey.party>
2019-01-23 18:38:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 INNER JOIN pledges ON projects.id = pledges.project_id
 GROUP BY pledges.project_id
 ORDER BY amnt_over DESC 
-ORDER BY title
 LIMIT 2;"
 end
 
